ac49a3b1432d3c3e45cf32496a3f3ae9f1781722
league <bap>
2024-08-07 11:01:39 -0700

    I work on the quiz game but still have a little more to do there. I learned a lot!

--- _03_print_and_popups/_d_quiz_game.py
+++ _03_print_and_popups/_d_quiz_game.py
@@ -8,18 +8,26 @@ if __name__ == '__main__':
     window.withdraw()
     # 1. Create a variable to hold the user's score. Set it equal to zero. 
     score = 0
-    messagebox.showinfo(message="your score is "+score+". good job!")
+    messagebox.showinfo(message="your score is "+str(score)+".")
     # ASK A QUESTION AND CHECK THE ANSWER
 
     #      // 2.  Ask the user a question 
-    question1 = simpledialog.askstring(title='1', prompt='how many colors are in the rainbow?')
+    question1 = simpledialog.askstring(title='one', prompt='how many people are in the League logo?')
     #      // 3.  Use an if statement to check if their answer is correct
-    if question1 == '7':
-        messagebox.showinfo(message='you got it right!')
+    if question1 == '5':
         score = 1
+        messagebox.showinfo(message='you got it right! your score is now '+str(score)+'!')
+    else:
+        messagebox.showinfo(message='oh no! you were wrong. ):')
+    question2 = simpledialog.askstring(title='two', prompt='what symbol is on the flag in the League logo?')
+    if question2 == 'lighting bolt':
+        score+=1
+        messagebox.showinfo(message='good job! you were right! your score is now '+str(score)+'!')
+    else:
+        messagebox.showinfo(message='it was a lightning bolt. good luck next time!')
+    question3 = simpledialog.askstring(title='three', prompt='what color is primarily used in the League logo?')
+    #      // 4.  if the user's answer was correct, add one to their score
 
-    #      // 4.  if the user's answer was correct, add one to their score 
- 
     # MAKE MORE QUESTIONS. Ask more questions by repeating the above 
     #      // Option: Subtract a point from their score for a wrong answer
  
